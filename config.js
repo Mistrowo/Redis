15aa676949fe01f9be716f73cdc6d8d898b9404e
Alex Parada <89418028+Mistrowo@users.noreply.github.com>
2024-05-03 12:49:55 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const redisHost = 'redis-19411.c283.us-east-1-4.ec2.redns.redis-cloud.com';
+const redisPort = 19411;
+const redisPassword = 'Wg8Y6L8yvqHddnmPmWSax22gPRWcJZoP';
+
+module.exports = {
+  redisHost,
+  redisPort,
+  redisPassword
+};
---
+++ test_redis.js
@@ -0,0 +1,37 @@
+const redis = require('redis');
+const { redisHost, redisPort, redisPassword } = require('./config');
+
+// Crear un cliente Redis
+const client = redis.createClient({
+    socket: {
+      host: redisHost,
+      port: redisPort
+    },
+    password: redisPassword
+  });
+
+// Manejar eventos de error y conexión
+client.on('error', (err) => console.log('Error de Redis Client', err));
+client.on('connect', () => console.log('Conexión exitosa con Redis'));
+
+// Función para insertar un valor en Redis
+async function setData(key, value) {
+  await client.set(key, value);
+  console.log(`Valor '${value}' insertado con la clave '${key}'`);
+}
+
+// Función para obtener un valor de Redis
+async function getData(key) {
+  const value = await client.get(key);
+  console.log(`Valor obtenido para la clave '${key}': ${value}`);
+}
+
+// Abrir la conexión
+client.connect();
+
+// Ejemplo de uso
+setData('miClave', 'Hola!');
+getData('miClave');
+
+// Cerrar la conexión cuando hayas terminado de realizar todas las operaciones
+client.quit();
